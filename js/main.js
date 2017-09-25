19793829f454a329b5ec0fb80759ecffbf4097eb
Eric Weiss <eweiss1995@gmail.com>
2017-09-25 00:32:34 -0400

    Flashing colors and many more images!

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
 $(document).ready(function() {
     easingFunctions();
+
+    // while (true) {
+        flashingColors();
+    // }
+
+    // $(window).scroll(function(){
+    //
+    //     // if($(this).scrolling() == true) {
+    //     //     $('#menu').fadeOut(500);
+    //     // }
+    //
+    //     if ($(this).scrollTop() > 400) {
+    //         $('#menu').fadeIn(500);
+    //     } else {
+    //         $('#menu').fadeOut(500);
+    //     }
+    //
+    //     // $('#menu').fadeOut(500);
+    // });
+
+    //Scrolling
+    $(window).scroll($.debounce( 250, true, function(){
+        $('#menu').fadeOut(500);
+    }));
+
+    //Not scrolling
+    $(window).scroll($.debounce( 250, function(){
+        if ($(this).scrollTop() > 400) {
+            $('#menu').fadeIn(500);
+        } else {
+            $('#menu').fadeOut(500);
+        }
+    }));
+
 });
 
 function easingFunctions() {
@@ -39,9 +73,61 @@ function easingFunctions() {
     setTimeout(function() {
             $(".ease-five")
                 .animate({
-                    opacity: 1
+                    opacity: 1,
                 }, 2000);
         }, 3000
     );
 
+}
+
+function flashingColors() {
+
+    var white = true;
+
+    for (var i = 5000; i < 100001; i = i + 5000) {
+        if (white) {
+            setTimeout(function () {
+                    $(".ease-five")
+                        .animate({
+                            color: "#FFFFFF"
+                        }, 250);
+                }, i
+            );
+
+            setTimeout(function () {
+                    $(".ease-five")
+                        .animate({
+                            color: "#191919"
+                        }, 1000);
+                }, i + 500
+            );
+
+            setTimeout(function () {
+                    $(".ease-five")
+                        .animate({
+                            color: "#FFFFFF"
+                        }, 250);
+                }, i + 2000
+            );
+            setTimeout(function () {
+                $(".ease-five")
+                    .animate({
+                        color: "#191919"
+                    }, 750);
+            }, i + 2500
+            );
+            white = false;
+        }
+        else
+        {
+            setTimeout(function () {
+                    $(".ease-five")
+                        .animate({
+                            color: "#191919"
+                        }, 1000);
+                }, i + 3000
+            );
+            white = true;
+        }
+    }
 }
